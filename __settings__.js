560652743c7e8593d1132169b75f199b58ef4b35
Goblin163cm <goblin163cm@iMac.local>
2022-11-19 12:59:20 +0800

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1584093.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 109843322, 110282382, 109848709, 109944840, 110246732, 110282716, 110320761, 110628911, 110746152 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/109848389/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/109848390/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/109848388/1/ammo.js', 'preload' : true},
+];
